28b9d06ce62e11b83d3a5c16dbbd561e2d39c4c4
yanzhang <sanchai20@163.com>
2025-10-21 18:00:34 +0800

    1

--- admin/launch.sh
+++ admin/launch.sh
@@ -5,3 +5,7 @@ set -e
 echo "Updating system and installing dependencies..."
 sudo apt update
 sudo apt install -y python3 python3-dev python3-venv libaugeas-dev gcc nginx build-essential
+
+
+
+sudo pkill -f litellm
