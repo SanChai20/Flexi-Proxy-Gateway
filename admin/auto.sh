b285f22f25979f2ae17701cf55cc9b8d9de7ce6b
yanzhang <sanchai20@163.com>
2025-11-18 11:44:09 +0800

    1

--- admin/auto.sh
+++ admin/auto.sh
@@ -67,7 +67,7 @@ validate_env() {
         "CF_Zone_ID"
         "FP_APP_TOKEN_PASS"
         "FP_PROXY_SERVER_OWNER"
-        "TOGETHERAI_API_KEY"
+        "FIREWORKS_AI_API_KEY"
     )
     
     local missing_vars=()
